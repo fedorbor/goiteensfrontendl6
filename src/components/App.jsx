135031df936b160fa651f392a3117b45d85c2229
fedorbor <112242236+fedorbor@users.noreply.github.com>
2024-05-25 11:31:44 +0300

    г

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
-import { Routes,Route } from 
+import { Routes,Route } from 'react-router-dom';
 export const App = () => {
   return (
     <div>
         <Routes>
-        <Route path="/" />
+        <Route path="/" element={<h1>heloo</h1>}/>
+        <Route path="/lessons" element={<h2>helos</h2>}/>
        </Routes>
     </div>
   );
